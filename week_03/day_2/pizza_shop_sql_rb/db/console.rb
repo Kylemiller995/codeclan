0f4838cc08d6acb3198375a526d415b0c4db254a
Craig Morton <CraigMorton22@gmail.com>
2017-08-16 09:36:16 +0100

    Update pizza end point

--- week_03/day_2/pizza_shop_sql_rb/db/console.rb
+++ week_03/day_2/pizza_shop_sql_rb/db/console.rb
@@ -4,17 +4,17 @@ require_relative("../models/pizza_order")
 PizzaOrder.delete_all()
 
 order1 = PizzaOrder.new({
-  "first_name"=> "Luke",
-  "last_name"=> "Skywalker",
-  "topping"=> "Wompa",
-  "quantity"=> "1"
+  "first_name" => "Luke",
+  "last_name" => "Skywalker",
+  "topping" => "Wompa",
+  "quantity" => "1"
 })
 
 order2 = PizzaOrder.new({
-  "first_name"=> "Darth",
-  "last_name"=> "Vader",
-  "topping"=> "Luke's Hand",
-  "quantity"=> "1"
+  "first_name" => "Darth",
+  "last_name" => "Vader",
+  "topping" => "Luke's Hand",
+  "quantity" => "1"
 })
 
 order1.save()
--- week_03/day_2/pizza_shop_sql_rb/models/pizza_order.rb
+++ week_03/day_2/pizza_shop_sql_rb/models/pizza_order.rb
@@ -15,69 +15,64 @@ class PizzaOrder
 
   def save()
     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
-    sql = "INSERT INTO pizza_orders
-      (
+    sql = '
+      INSERT INTO pizza_orders (
         first_name,
         last_name,
         topping,
         quantity
+      ) VALUES (
+        $1, $2, $3, $4
       )
-      VALUES
-      (
-        $1,$2,$3,$4
-      )
-      RETURNING id;"
+      RETURNING id;'
+      # 'RETURNING id' or 'RETURNING *' will achieve the same thing for us in this situation
     values = [@first_name, @last_name, @topping, @quantity]
-    db.prepare("save", sql)
-    @id = db.exec_prepared("save", values)[0]["id"].to_i()
+    db.prepare('save', sql)
+    saved_order_hash = db.exec_prepared('save', values)[0]
+    @id = saved_order_hash['id'].to_i()
     db.close()
   end
 
   def update()
     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
-    sql = "UPDATE pizza_orders
-      SET
-      (
+    sql = '
+      UPDATE pizza_orders SET (
         first_name,
         last_name,
         topping,
         quantity
-      ) =
-      (
+      ) = (
         $1, $2, $3, $4
       )
-      WHERE id = $5;"
+      WHERE id = $5;'
     values = [@first_name, @last_name, @topping, @quantity, @id]
-    db.prepare("update", sql)
-    db.exec_prepared("update", values)
+    db.prepare('update', sql)
+    db.exec_prepared('update', values)
     db.close()
   end
 
   def delete()
     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
-    sql =  "DELETE FROM pizza_orders
-      WHERE id = $1;"
+    sql = 'DELETE FROM pizza_orders WHERE id = $1;'
     values = [@id]
-    db.prepare("delete", sql)
-    db.exec_prepared("delete", values)
+    db.prepare('delete', sql)
+    db.exec_prepared('delete', values)
     db.close()
   end
 
   def PizzaOrder.delete_all()
     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
-    sql = "DELETE FROM pizza_orders;"
-    values = []
-    db.prepare("delete_all", sql)
-    db.exec_prepared("delete_all", values)
+    sql = 'DELETE FROM pizza_orders;'
+    db.prepare('delete_all', sql)
+    db.exec_prepared('delete_all', [])
     db.close()
   end
 
   def PizzaOrder.all()
     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
-    sql = "SELECT * FROM pizza_orders;"
-    values = []
-    db.prepare("all", sql)
-    orders = db.exec_prepared("all", values)
+    sql = 'SELECT * FROM pizza_orders;'
+    db.prepare('all', sql)
+    orders = db.exec_prepared('all', [])
     db.close()
     return orders.map { |order_hash| PizzaOrder.new( order_hash ) }
   end
