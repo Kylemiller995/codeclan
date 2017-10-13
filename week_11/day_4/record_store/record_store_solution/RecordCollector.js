a2c0ee5a518469443b9b6884d245317bf76866fb
Vismajor <podobaszalaizsolt@gmail.com>
2017-10-13 13:59:15 +0100

    added solution final for recordstore lab

--- week_11/day_4/record_store/record_store_solution/RecordCollector.js
+++ week_11/day_4/record_store/record_store_solution/RecordCollector.js
@@ -1,4 +1,5 @@
-function RecordCollector(cash) {
+function RecordCollector(name, cash) {
+  this.name = name;
   this.collection = [];
   this.cash = cash;
 }
@@ -16,6 +17,44 @@ RecordCollector.prototype.sellRecord = function (record) {
   this.collection.splice(recordIndex, 1);
 
   this.cash += record.price;
+}
+
+RecordCollector.prototype.totalValue = function(){
+  var totalValue = this.collection.reduce(function(sum, record){
+    return sum + record.price;
+  }, 0)
+  return totalValue;
+}
+
+RecordCollector.prototype.totalValueByGenre = function(genre){
+  var recordsByGenre = this.collection.filter(function(record){
+    return record.genre === genre;
+  })
+  var valueByGenre = recordsByGenre.reduce(function(sum, record){
+    return sum + record.price;
+  }, 0)
+  return valueByGenre;
+
+}
+
+RecordCollector.prototype.sortRecordsByValue = function (){
+  this.collection = this.collection.sort(function(a,b){
+    return b.price - a.price;
+  })
+  return this.collection;
+}
+
+RecordCollector.prototype.mostValuableRecord = function() {
+  var sortedCollection = this.sortRecordsByValue();
+  return sortedCollection[0];
+}
+
+RecordCollector.prototype.compareCollection = function(collectorToCompare) {
+  if(this.totalValue() > collectorToCompare.totalValue()){
+    return this.name + "'s collection is more valuable";
+  } else {
+    return collectorToCompare.name + "'s collection is more valuable";
+  }
 }
 
 module.exports = RecordCollector;
--- week_11/day_4/record_store/record_store_solution/specs/RecordCollectorTests.js
+++ week_11/day_4/record_store/record_store_solution/specs/RecordCollectorTests.js
@@ -3,7 +3,7 @@ var RecordCollector = require("../RecordCollector.js");
 var RecordStore = require("../RecordStore.js");
 var Record = require("../Record.js");
 
-describe("Record Collector Tests", function() {
+describe("Record Collector", function() {
 
   var recordStore;
   var record;
@@ -12,40 +12,78 @@ describe("Record Collector Tests", function() {
 
   beforeEach("Setup", function() {
     recordStore = new RecordStore("Zsolt Healy's Records", "Edinburgh");
-    record = new Record("Nirvana", "Nevermind", "grunge", 9.99);
+    record = new Record("Nirvana", "Nevermind", "grunge", 10);
     record2 = new Record("Red Hot Chilli Peppers", "By The Way", "rock", 7.50);
-    recordCollector = new RecordCollector(8.00);
+    record3 = new Record("AC/DC", "Highway to hell", "rock", 7.50);
+    recordCollector = new RecordCollector("Craig", 8.00);
   });
 
-  it("Record collector's has cash of 8.00", function() {
-    assert.equal(recordCollector.cash, 8.00);
+  it("has cash of 8.00", function() {
+    assert.strictEqual(recordCollector.cash, 8.00);
   });
 
-  it("Record collector can buy a record", function() {
+  it("can buy a record", function() {
     recordCollector.buyRecord(record2);
-    assert.equal(recordCollector.collection.length, 1);
+    assert.strictEqual(recordCollector.collection.length, 1);
   });
 
-  it("Record collector's cash goes down when buying record", function() {
+  it("cash goes down when buying record", function() {
     recordCollector.buyRecord(record2);
-    assert.equal(recordCollector.cash, 0.50);
+    assert.strictEqual(recordCollector.cash, 0.50);
   });
 
-  it("Record can't buy record if not enough cash", function() {
-    assert.equal(recordCollector.buyRecord(record), "Sorry you can't afford this record");
-    assert.equal(recordCollector.collection.length, 0);
+  it("can't buy record if not enough cash", function() {
+    assert.strictEqual(recordCollector.buyRecord(record), "Sorry you can't afford this record");
+    assert.strictEqual(recordCollector.collection.length, 0);
   });
 
-  it("Record Collector can sell record", function() {
-    recordCollector.buyRecord(record2);
-    recordCollector.sellRecord(record2);
-    assert.equal(recordCollector.collection.length, 0);
-  });
-
-  it("Record Collectors cash increases when selling", function() {
+  it("can sell record", function() {
     recordCollector.buyRecord(record2);
     recordCollector.sellRecord(record2);
-    assert.equal(recordCollector.cash, 8.00);
+    assert.strictEqual(recordCollector.collection.length, 0);
+  });
+
+  it("cash increases when selling", function() {
+    recordCollector.buyRecord(record2);
+    recordCollector.sellRecord(record2);
+    assert.strictEqual(recordCollector.cash, 8.00);
+  });
+
+  it("can find total value of collection", function() {
+    recordCollector.cash = 100
+    recordCollector.buyRecord(record);
+    recordCollector.buyRecord(record2);
+    assert.strictEqual(recordCollector.totalValue(), 17.50);
+  });
+
+  it("can find total value of a given genre", function() {
+    recordCollector.cash = 100
+    recordCollector.buyRecord(record2);
+    recordCollector.buyRecord(record3);
+    assert.strictEqual(recordCollector.totalValueByGenre("rock"), 15);
+  });
+
+  it("can sort their records by value", function() {
+    recordCollector.cash = 100
+    recordCollector.buyRecord(record2);
+    recordCollector.buyRecord(record);
+    assert.deepStrictEqual(recordCollector.sortRecordsByValue(), [record, record2]);
+  });
+
+  it("can find the most valuable record", function() {
+    recordCollector.cash = 100
+    recordCollector.buyRecord(record);
+    recordCollector.buyRecord(record3);
+    assert.deepStrictEqual(recordCollector.mostValuableRecord(), record);
+  });
+
+  it("can compare value with another collector", function() {
+    recordCollector.cash = 100
+    recordCollector.buyRecord(record);
+    recordCollector.buyRecord(record3);
+    var recordCollector2 = new RecordCollector("Alex", 10);
+    recordCollector2.buyRecord(record2);
+    assert.strictEqual(recordCollector.compareCollection(recordCollector2), "Craig's collection is more valuable");
   });
 
 })
